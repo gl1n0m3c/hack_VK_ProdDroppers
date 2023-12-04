887042c24f9d12dacc49ba13f247053bd77e1ae5
Nicolashka <nperewerzew2005@yandex.ru>
2023-12-05 00:14:04 +0300

    BASE_OPTIONS

---
+++ models/database.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+
+load_dotenv()
+
+
+Base = declarative_base()
+
+SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_PATH")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
