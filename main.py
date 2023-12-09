7f932cf3094bb7b6f5e4c7df56e6891828e6dba6
Nicolashka <nperewerzew2005@yandex.ru>
2023-12-09 23:58:05 +0300

    test sockets

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from routers.friends import router as friends_router
 from routers.rooms import router as rooms_router
 from routers.users import router as users_router
+from routers.websockets.rooms import router as websocket_rooms_router
 
 
 app = FastAPI()
@@ -11,3 +12,5 @@ app = FastAPI()
 app.include_router(friends_router, prefix="/api/friends", tags=["friends"])
 app.include_router(rooms_router, prefix="/api/rooms", tags=["rooms"])
 app.include_router(users_router, prefix="/api/users", tags=["users"])
+
+app.include_router(websocket_rooms_router, prefix="/ws/room")
